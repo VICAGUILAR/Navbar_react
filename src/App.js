2590ee80193617b5cde22af7481db8b05224e8e4
VICAGUILAR <isc_vhpa@hotmail.com>
2022-05-16 22:52:12 -0500

    Subiendo navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navegacion from './componentes/Navegacion';
+
+function App() {
+  return (
+    <div className="App">
+      <Navegacion />
+    </div>
+  );
+}
+
+export default App;

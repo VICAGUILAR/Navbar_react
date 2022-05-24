d7056fc7db23b8024dad9e0f6373e6c3eb0252b6
VICAGUILAR <isc_vhpa@hotmail.com>
2022-05-24 16:42:49 -0500

    boton con contador

---
+++ src/componentes/ItemCount.js
@@ -0,0 +1,30 @@
+
+
+import React, {useState} from 'react';
+
+import '../ItemCount.css';
+export const ItemCount = ({initial, stock, onAdd}) => {
+    const [count, setCount] = useState(initial);
+
+    const decrease = () => {
+        setCount(count - 1);
+    }
+
+    const increase = () => {
+        setCount(count + 1);
+    }
+
+    return (
+        <div class= 'counter'>
+        <button class="btn btn-dark" disabled={count <= 1} onClick={decrease}>-</button>
+        <span>{count}</span>
+        <button class="btn btn-dark" disabled={count >= stock} onClick={increase}>+</button>
+        <div>
+            <button class="btn btn-danger">Agregar al Carrito</button>
+        </div>
+
+    </div>
+    ); 
+}
+
+export default ItemCount;
--- src/componentes/ItemListConteiner.js
+++ src/componentes/ItemListConteiner.js
@@ -1,9 +1,18 @@
 
+import ItemCount from '../componentes/ItemCount'
 
 function  ItemListeConteiner () {
+
+    const onAdd= (quantity) => {
+        console.log (`Compraste ${quantity} unidades`);
+    }
+
     return (
         <div className="ItemListConteiner">
             ESTOS PRODUCTOS TE PUEDEN INTERESAR
+
+            <ItemCount initial={1} stock={5} onAdd={onAdd}/>
+            
         </div>
     );
 }
